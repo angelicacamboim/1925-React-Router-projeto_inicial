bc81f37ccb32f24b9c019e3f3c28d1dd04e3ae69
Angelica Bayer <angelicacamboim@gmail.com>
2021-04-26 16:36:41 -0300

    page 404 created

--- petshop/src/App.jsx
+++ petshop/src/App.jsx
@@ -4,6 +4,7 @@ import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 
 import Home from './paginas/Home'
 import Sobre from './paginas/Sobre'
+import Pagina404 from './paginas/Pagina404'
 
 function App() {
   
@@ -17,6 +18,11 @@ function App() {
         <Route path='/sobre'>
           <Sobre />
         </Route>
+
+        <Route>
+          <Pagina404 />
+        </Route>
+
       </Switch>
     </Router>
   )
